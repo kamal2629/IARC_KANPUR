f4bd71fd07e98f1c1dd40c046769b813b1154e6c
pawarharshit <49332515+pawarharshit@users.noreply.github.com>
2020-02-27 00:24:23 +0530

    I2c device  address finding code is added

--- print.ino
+++ print.ino
@@ -1,20 +1,40 @@
-/* I2C LCD with Arduino example code. More info: https://www.makerguides.com */
-// Include the libraries:
-// LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C
-#include <Wire.h> // Library for I2C communication
-#include <LiquidCrystal_I2C.h> // Library for LCD
-// Wiring: SDA pin is connected to A4 and SCL pin to A5.
-// Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // Change to (0x27,16,2) for 16x2 LCD.
+/*I2C_scanner
+  This sketch tests standard 7-bit addresses.
+  Devices with higher bit address might not be seen properly.*/
+  
+#include <Wire.h>
 void setup() {
-  // Initiate the LCD:
-  lcd.init();
-  lcd.backlight();
+  Wire.begin();
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("\nI2C Scanner");
 }
 void loop() {
-  // Print 'Hello World!' on the first line of the LCD:
-  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
-  lcd.print("Hello World!"); // Print the string "Hello World!"
-  lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
-  lcd.print("LCD tutorial");
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for (address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+      nDevices++;
+    }
+    else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+  delay(5000);
 }
